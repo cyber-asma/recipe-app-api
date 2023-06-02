f2f48687037c7fa6ecc67ae461383ebdd9ce4d42
Asma-cyber <asma.aha666@gmail.com>
2023-06-02 11:02:09 +0100

    added some tets

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+This is a simple calculator module
+"""
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
---
+++ app/app/test.py
@@ -0,0 +1,16 @@
+"""
+simple test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test for calc app"""
+    def test_add(self):
+        """Test add function"""
+        self.assertEqual(calc.add(3, 8), 11)
+    def test_subtract(self):
+        """Test subtract function"""
+        self.assertEqual(calc.subtract(11, 5), 6)
