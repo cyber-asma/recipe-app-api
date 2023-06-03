df67bcd7bf94e425895e6d936935d8462a94287d
Asma-cyber <asma.aha666@gmail.com>
2023-06-04 00:26:43 +0100

    done some migration and put test for models

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core', 
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'core.User'
