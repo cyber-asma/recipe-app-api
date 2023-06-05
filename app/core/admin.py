fdd33d321452c0de5d466f55be2d25b3b265aa55
Asma-cyber <asma.aha666@gmail.com>
2023-06-05 17:39:34 +0100

    implemented the cration and mofication of superuser

---
+++ app/core/admin.py
@@ -0,0 +1,32 @@
+"""
+Django admin customizations
+"""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+admin.site.register(models.user, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+"""
+Test for the django admin modifications.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+    """Test admin site"""
+
+    def setUp(self):
+        """Setup function for the tests"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@example.com',
+            password = 'test123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'test123',
+            name = 'Test User',
+        )
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        # print(url)
+        res = self.client.get(url)
+        # print(res)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_edit_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # print(url)
+        res = self.client.get(url)
+        # print(res)
+        self.assertEqual(res.status_code, 200)
